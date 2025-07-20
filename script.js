731cba7a71d1e4d2f5305da9d0dc73eca351c6df
Тхатель Алибек <alibek.tkhatel@mail.ru>
2025-07-20 23:44:50 +0300

    successful lesson-02

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-console.log("Hello World!");
+// Урок 1
+// console.log('Hello World!');
 
-alert("Hello World!")
+// alert('Hello World!');
+
+// Урок 2
+
+let firstName = 'Алибек';
+let lastName = 'Тхатель';
+let patronymic = 'Шумафович';
+
+console.log(firstName);
+console.log(lastName);
+console.log(patronymic);
+
