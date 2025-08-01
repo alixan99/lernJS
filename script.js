0b0fd75aed96ba8ef8a1967f55169f32619eff26
Тхатель Алибек <alibek.tkhatel@mail.ru>
2025-08-02 01:30:46 +0300

    task calculated successful

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-console.log("Hello World!");
+/* let intOne = +prompt('Введите первое число');
+let intTwo = +prompt('Введите второе число');
 
-alert("Hello World!")
+console.log(
+  `Результаты вычислений: \nСумма ${intOne + intTwo}\nРазность ${
+    intOne - intTwo
+  }\nПроизведение ${intOne * intTwo}\nЧастное ${intOne / intTwo}`,
+); */
+
+function calculation() {
+  let intOne = +prompt('Введите первое число');
+  let intTwo = +prompt('Введите второе число');
+
+  let sum = intOne + intTwo;
+  let dif = intOne - intTwo;
+  let multi = intOne * intTwo;
+  let div = intOne / intTwo;
+
+  return console.log(
+    `Результаты вычислений: \nСумма ${sum}\nРазность ${dif}\nПроизведение ${multi}\nЧастное ${div}`,
+  );
+}
+
+calculation();
